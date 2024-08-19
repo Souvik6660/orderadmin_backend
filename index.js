77e2fc5237e1495e4e395c160e6df28938ebdd73
souvik Das <souvikdas6660@gmail.com>
2024-08-19 10:28:19 +0530

    changes2

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const serviceroute=require('./router/service.router')
 const adminRoute=require("./router/admin.router");
 const errorMiddleware = require('./middlewares/error.middleware');
 
-const corsOptions = {
-  origin: ["http://localhost:5175", "http://localhost:5173"], // Allow both origins
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: ["http://localhost:5175", "http://localhost:5173"], // Allow both origins
+//   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 
