a8c4a3f834692e86d4a9df46c8347650d658fbed
souvik Das <souvikdas6660@gmail.com>
2024-08-19 10:08:33 +0530

    changes

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/api/admin",adminRoute);
 // Place your error middleware at the end
 app.use(errorMiddleware);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     await connectDb();
